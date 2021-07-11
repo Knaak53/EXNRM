16adf5d2cfcf9117675a3eebe890d1dd5fa47bff
Knatus53 <manugara53@gmail.com>
2021-07-11 22:01:07 +0200

    removing uneeded

--- [esx]/[skins]/esx_skin.back/locales/es.lua
+++
@@ -1,6 +0,0 @@
-Locales['es'] = {
-  ['skin_menu'] = 'Menú de Personalizacion',
-  ['use_rotate_view'] = 'Pulsa ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ y ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ para rotar la camara.',
-  ['skin'] = 'Cambiar aspecto',
-  ['saveskin'] = 'Guardar aspecto en un archivo',
-}
