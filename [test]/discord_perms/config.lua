097f722327e5a15e9ef9bb7dbac484a796210920
Knatus53 <manugara53@gmail.com>
2021-07-12 02:21:46 +0200

    .

--- [test]/discord_perms/config.lua
+++
@@ -1,9 +0,0 @@
-Config = {
-	DiscordToken = "",
-	GuildId = "",
-
-	-- Format: ["Role Nickname"] = "Role ID" You can get role id by doing \@RoleName
-	Roles = {
-		["▸👨‍💼┊Whitelist"] = "" -- This would be checked by doing exports.discord_perms:IsRolePresent(user, "TestRole")
-	}
-}
